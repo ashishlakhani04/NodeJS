be4dbbad12a1632147b014d0e22a9df09249e16d
Ashish Lakhani <depashlakhani@gmail.com>
2018-04-18 09:35:59 +0530

    Add Lodash Dependencies

--- NodeJSProjects/1.Notes_Node/app.js
+++ NodeJSProjects/1.Notes_Node/app.js
@@ -7,10 +7,10 @@ const notes = require('./notes');
 var user = os.userInfo();
 // console.log(user);
 
-fs.appendFile('greetings.txt',`Hello ${user.username} . You are ${notes.age} years old!
-				 This means ${notes.check()} .`,function(err){ // template strings (es6)
-	if(err){
-		console.log('Unable to load the file!');
-	}
-});
+// fs.appendFile('greetings.txt',`Hello ${user.username} . You are ${notes.age} years old!
+// 				 This means ${notes.check()} .`,function(err){ // template strings (es6)
+// 	if(err){
+// 		console.log('Unable to load the file!');
+// 	}
+// });
  // fs.appendFileSync('greetings.txt','Hello World!');
