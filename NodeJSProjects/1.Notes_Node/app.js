63428593ae6ffdc8e44208b37cc9c5c1fe92a695
Ashish Lakhani <depashlakhani@gmail.com>
2018-04-18 10:18:36 +0530

    delete node_modules

--- NodeJSProjects/1.Notes_Node/app.js
+++ NodeJSProjects/1.Notes_Node/app.js
@@ -1,10 +1,25 @@
+// first, we have included core node modules like OS,fileSystem
+// secondly, we have included our own files like notes.js
+// lastly, we have installed npm with commands like npm init and npm install and install
+// lodash which contains some basic utilities functions
+
+
+
 console.log('Starting App.js!');
 
 const fs = require('fs');
 const os = require('os');
-const notes = require('./notes');
+const _ = require('lodash');
+// const notes = require('./notes');
 
-var user = os.userInfo();
+
+// console.log(_.isString('true'));
+
+var filteredArray = _.uniq([1,2,5,4,1,2,5,6,5]);
+console.log(filteredArray);
+
+
+// var user = os.userInfo();
 // console.log(user);
 
 // fs.appendFile('greetings.txt',`Hello ${user.username} . You are ${notes.age} years old!
